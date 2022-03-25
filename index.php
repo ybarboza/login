90c5060847d3f04dc17795bdb1f6cdf934bdd247
Yago Barbosa <sciencieboy@gmail.com>
2022-03-24 22:55:06 -0400

    New

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="band">
+        <img src="imagens/mascara.png" alt="" width="190">
+        <form action="dados.php">
+            <input type="email" name="email" id="" placeholder="E-mail">
+            <input type="password" name="senha" id="" placeholder="Senha">
+            <input type="submit" value="Enviar" style="width:150px;">
+        </form>
+        <div class="text">
+            <a href="">Criar conta</a>
+            <a href="">Esqueceu sua senha?</a>
+       </div>
+    </div>
+</body>
+</html>
